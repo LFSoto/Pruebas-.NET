fc19b8a662202612086b266c260a54f8c8294148
Gustavo-Montero <gmonterog@ucenfotec.ac.cr>
2026-02-25 23:33:06 -0600

    parte 1 tarea

--- AutomationPracticeDemo.Tests/Pages/FormPage.cs
+++ AutomationPracticeDemo.Tests/Pages/FormPage.cs
@@ -1,4 +1,6 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace AutomationPracticeDemo.Tests.Pages
 {
@@ -17,17 +19,94 @@ namespace AutomationPracticeDemo.Tests.Pages
         private IWebElement CountryDropdown => _driver.FindElement(By.Id("country"));
         private IWebElement SubmitButton => _driver.FindElement(By.ClassName("submit-btn"));
 
-        public void FillForm(string name, string email, string phone, string country)
+        private IWebElement Alertsimplebutton => _driver.FindElement(By.Id("alertBtn"));
+        private IWebElement AddressInput => _driver.FindElement(By.Id("textarea"));
+        private IWebElement GenderInput => _driver.FindElement(By.Id("female"));
+        private IWebElement CheckboxMondayInput => _driver.FindElement(By.Id("monday"));
+        private IWebElement CheckboxTuesdayInput => _driver.FindElement(By.Id("tuesday"));
+        private IWebElement CheckboxwednesdaydInput => _driver.FindElement(By.Id("wednesday"));
+
+        private IWebElement ColorsInput => _driver.FindElement(By.Id("colors"));
+
+        private IWebElement SortedListInput => _driver.FindElement(By.Id("animals"));
+
+        private IWebElement DatePicker1Input => _driver.FindElement(By.Id("datepicker"));
+
+        public void FillForm(string name, string email, string phone, string address)
+
         {
             NameInput.SendKeys(name);
             EmailInput.SendKeys(email);
             PhoneInput.SendKeys(phone);
-            CountryDropdown.SendKeys(country);
+            AddressInput.SendKeys(address);
+            
+
         }
 
         public void Submit()
         {
             SubmitButton.Click();
+        }
+
+        public void alertsimple()
+        {
+            Alertsimplebutton.Click();
+        }
+        public void Gender()
+        {
+            GenderInput.Click();
+        }
+        public void days()
+        {
+            CheckboxMondayInput.Click();
+            CheckboxTuesdayInput.Click();
+            CheckboxwednesdaydInput.Click();
+        }
+        public void country()
+        {
+            SelectElement select = new SelectElement(CountryDropdown);
+            select.SelectByText("Japan");
+
+        }
+        public void Colors()
+        {
+            SelectElement select = new SelectElement(ColorsInput);
+            select.SelectByText("Red");
+
+        }
+        public void Animals()
+        {
+            SelectElement select = new SelectElement(SortedListInput);
+            select.SelectByText("Dog");
+
+        }
+
+        public void Datapicker1()
+        {
+            DatePicker1Input.Click();
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            // Esperar a que el datepicker (generalmente renderizado en #ui-datepicker-div) sea visible
+            wait.Until(d => d.FindElement(By.CssSelector("#ui-datepicker-div")).Displayed);
+
+            // Navegar hasta febrero 2025 usando el contenedor global del datepicker
+            while (true)
+            {
+                string header = _driver.FindElement(By.CssSelector("#ui-datepicker-div .ui-datepicker-title")).Text;
+                if (header.Contains("February") && header.Contains("2027"))
+                    break;
+                _driver.FindElement(By.CssSelector("#ui-datepicker-div .ui-datepicker-next")).Click();
+                // esperar a que el encabezado se actualice
+                wait.Until(d => d.FindElement(By.CssSelector("#ui-datepicker-div .ui-datepicker-title")).Displayed);
+            }
+
+            // Seleccionar el día 10 dentro del contenedor global
+            _driver.FindElement(By.XPath("//div[@id='ui-datepicker-div']//table[@class='ui-datepicker-calendar']//a[text()='10']")).Click();
+        }
+        public void Datapicker2()
+        {
+        } public void Datapicker3()
+        {
         }
     }
 }
--- AutomationPracticeDemo.Tests/Tests/FormTest.cs
+++ AutomationPracticeDemo.Tests/Tests/FormTest.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using AutomationPracticeDemo.Tests.Pages;
 using AutomationPracticeDemo.Tests.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V137.BluetoothEmulation;
 
 namespace AutomationPracticeDemo.Tests.Tests
 {
@@ -10,11 +12,59 @@ namespace AutomationPracticeDemo.Tests.Tests
         public void Should_FillAndSubmitForm()
         {
             var formPage = new FormPage(Driver);
-            formPage.FillForm("Juan Perez", "juan@test.com", "88888888", "Costa Rica");
+            formPage.FillForm("Gustavo Montero Gonzalez", "gumontero@test.com", "88651273", "Coyol, Alajuela");
+            formPage.Gender();
+            Thread.Sleep(3000);
+            ScreenshotHelper.TakeScreenshot(Driver, "gender.png");
+
+            formPage.days();
+            Thread.Sleep(3000);
+            ScreenshotHelper.TakeScreenshot(Driver, "days.png");
+
+            formPage.country();
+            Thread.Sleep(3000);
+            ScreenshotHelper.TakeScreenshot(Driver, "country.png");
+
+            formPage.Colors();
+             Thread.Sleep(3000); 
+            ScreenshotHelper.TakeScreenshot(Driver, "colors.png");  
+
+            formPage.Animals();
+                Thread.Sleep(3000); 
+            ScreenshotHelper.TakeScreenshot(Driver, "animals.png");
+
+
+            formPage.Datapicker1();
+            Thread.Sleep(3000);
+            ScreenshotHelper.TakeScreenshot(Driver, "datepicker1.png");
+           
+
             formPage.Submit();
 
             ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
             Assert.Pass("Formulario llenado y enviado.");
+        }
+
+        [Test]
+        public void Should_Alertsimple()
+        {
+            var formPage = new FormPage(Driver);
+
+            formPage.alertsimple();
+           
+
+            IAlert alert = Driver.SwitchTo().Alert();
+
+
+            string alertText = alert.Text;
+
+            Assert.That(alertText, Is.EqualTo("I am an alert box!"));
+            
+            Thread.Sleep(3000);
+            alert.Accept();
+            ScreenshotHelper.TakeScreenshot(Driver, "Alertsimple.png");
+
+
         }
     }
 }
