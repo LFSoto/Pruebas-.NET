ce8b47d88739927180d9872f82bfcd35dd3cc2d3
Daya <dayana.pm22@gmail.com>
2026-02-25 18:13:50 -0600

    Tarea 1 - Dayana

--- CalculadoraLib/Calculadora.cs
+++ CalculadoraLib/Calculadora.cs
@@ -2,15 +2,50 @@
 
 public class Calculadora
 {
-    public int Sumar(int a, int b) => a + b;
+    private readonly ITimeProvider _timeProvider;
 
-    public int Restar(int a, int b) => a - b;
+    public Calculadora(ITimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+      
+    }
 
-    public int Multiplicar(int a, int b) => a * b;
+    public void ValidarHorario()
+    {
+        var horaActual = _timeProvider.Now.TimeOfDay;
+        var inicio = new TimeSpan(8, 0, 0);
+        var fin = new TimeSpan(18, 0, 0);
+
+        if (horaActual < inicio || horaActual > fin)
+        {
+            throw new InvalidOperationException("Fuera de horario, la operación no es permitida");
+        }
+    
+    }
+
+    public int Sumar(int a, int b)
+    {
+        ValidarHorario();
+        return a + b;
+    }
+
+    public int Restar(int a, int b)
+    {
+        ValidarHorario();
+        return a - b;
+    }
+
+    public int Multiplicar(int a, int b)
+    {
+        ValidarHorario();
+        return a * b;
+    }
 
     public double Dividir(int a, int b)
     {
+        ValidarHorario();
         if (b == 0) throw new DivideByZeroException("No se puede dividir entre cero.");
+        //return a / b;
         return (double)a / b;
     }
 }
--- CalculadoraTests/CalculadoraServiceTest.cs
+++ CalculadoraTests/CalculadoraServiceTest.cs
@@ -7,21 +7,57 @@ namespace CalculadoraTests;
 [TestFixture]
 public class CalculadoraServiceTests
 {
+    private Mock<ITimeProvider> _timeProviderMock;
+    private Calculadora calc;
+
+    [SetUp]
+
+    public void Setup()
+    {
+        _timeProviderMock = new Mock<ITimeProvider>();
+    }
+
     [Test]
     public void SumarYGuardar_DeberiaGuardarOperacionEnRepositorio()
     {
-        // Arrange
+        // Arrange - Preparar
         var repoMock = new Mock<IOperacionRepository>();
-        var calc = new Calculadora();
+        //var calc = new Calculadora();
         var service = new CalculadoraService(calc, repoMock.Object);
 
-        // Act
+        // Act - Actuar
         var resultado = service.SumarYGuardar(2, 3);
 
-        // Assert
+        // Assert - Afirmar
         Assert.AreEqual(5, resultado);
 
         // Verificamos que se haya llamado al método GuardarOperacion
         repoMock.Verify(r => r.GuardarOperacion("2 + 3", 5), Times.Once);
+    }
+
+    [Test]
+
+    public void DentroDelHorarioPermitido_DeberiaRetornarElResultadoCorrecto()
+    {
+        _timeProviderMock.Setup(tp => tp.Now).Returns(new DateTime(2026, 2, 19, 10, 0, 0));
+        calc = new Calculadora(_timeProviderMock.Object);
+
+        // Act - Actuar
+        var resultado = calc.Sumar(5, 3);
+
+        // Assert - Afirmar
+        Assert.AreEqual(8, resultado);
+    }
+
+    [Test]
+
+    public void FueraDelHorarioPermitido_DeberiaLanzarUnaExcepcion()
+    {
+        _timeProviderMock.Setup(tp => tp.Now).Returns(new DateTime(2026, 2, 19, 20, 0, 0));
+        calc = new Calculadora(_timeProviderMock.Object);
+
+        // Act & Assert - Actuar y Afirmar
+        Assert.Throws<InvalidOperationException>(() => calc.Sumar(5, 3));
+
     }
 }
