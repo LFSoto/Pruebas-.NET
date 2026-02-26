d64c969a6b56fc1382868bcdb573036f9482cdfd
LFSoto <soto2197@gmail.com>
2026-02-25 22:03:20 -0600

    Screenshot helper

--- AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
+++ AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
@@ -1,4 +1,6 @@
 using OpenQA.Selenium;
+using System;
+using System.IO;
 
 namespace AutomationPracticeDemo.Tests.Utils
 {
@@ -6,8 +8,15 @@ namespace AutomationPracticeDemo.Tests.Utils
     {
         public static void TakeScreenshot(IWebDriver driver, string fileName)
         {
+            var projectRoot = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
+            var screenshotsDir = Path.Combine(projectRoot, "screenshots");
+            Directory.CreateDirectory(screenshotsDir);
+
+            var timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff");
+            var fullPath = Path.Combine(screenshotsDir, $"{timestamp}_{fileName}.png");
+
             var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-            screenshot.SaveAsFile(fileName);
+            screenshot.SaveAsFile(fullPath);
         }
     }
 }
