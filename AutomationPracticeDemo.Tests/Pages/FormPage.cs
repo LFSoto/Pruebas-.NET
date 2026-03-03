764a7562959bbc24594d046b0504088f29c6c8f0
FerSalazar <51107427+FerSalazar@users.noreply.github.com>
2026-03-02 20:42:21 -0600

    Cambios Practica semana 2
    Se realizan pruebas automatizadas

--- AutomationPracticeDemo.Tests/Pages/FormPage.cs
+++ AutomationPracticeDemo.Tests/Pages/FormPage.cs
@@ -1,4 +1,6 @@
 using OpenQA.Selenium;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace AutomationPracticeDemo.Tests.Pages
 {
@@ -16,7 +18,12 @@ namespace AutomationPracticeDemo.Tests.Pages
         private IWebElement PhoneInput => _driver.FindElement(By.Id("phone"));
         private IWebElement CountryDropdown => _driver.FindElement(By.Id("country"));
         private IWebElement SubmitButton => _driver.FindElement(By.ClassName("submit-btn"));
-
+        private IWebElement GenderInput => _driver.FindElement(By.Name("gender"));
+        private IList<IWebElement> DaysCheckBox => _driver.FindElements(By.XPath("//input[@type='checkbox' and @class='form-check-input']"));
+        private IWebElement StartDateInput => _driver.FindElement(By.Id("start-date"));
+        private IWebElement EndDateInput => _driver.FindElement(By.Id("end-date"));
+        private IWebElement SubmitDateButton => _driver.FindElement(By.CssSelector(".date-picker-box .submit-btn"));
+        private IWebElement PromptButton => _driver.FindElement(By.Id("promptBtn"));
         public void FillForm(string name, string email, string phone, string country)
         {
             NameInput.SendKeys(name);
@@ -24,10 +31,39 @@ namespace AutomationPracticeDemo.Tests.Pages
             PhoneInput.SendKeys(phone);
             CountryDropdown.SendKeys(country);
         }
-
+        public void FillGender(string gander)
+        {
+            GenderInput.SendKeys(gander);
+            GenderInput.Click();
+        }
         public void Submit()
         {
             SubmitButton.Click();
+        }
+        public void FillDays(string day)
+        {
+            foreach (IWebElement item in DaysCheckBox)
+            {
+                if(item.GetAttribute("value") == day) { 
+                    item.SendKeys(day);
+                    item.Click();
+                }
+            }
+        }
+        public void FillRangeDates(string start, string end)
+        {
+            StartDateInput.Clear();
+            StartDateInput.SendKeys(start);
+            EndDateInput.Clear();
+            EndDateInput.SendKeys(end);
+        }
+        public void SubmitDates()
+        {
+            SubmitDateButton.Click();
+        }
+        public void OpenAlertPrompt()
+        {
+            PromptButton.Click();
         }
     }
 }
--- AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
+++ AutomationPracticeDemo.Tests/Utils/ScreenshotHelper.cs
@@ -1,13 +1,41 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.BrowsingContext;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace AutomationPracticeDemo.Tests.Utils
 {
     public static class ScreenshotHelper
     {
-        public static void TakeScreenshot(IWebDriver driver, string fileName)
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+
+        public static void TakeScreenshot(IWebDriver driver, string fileName, bool alert = false)
         {
-            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-            screenshot.SaveAsFile(fileName);
+            var rutaFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\", "Evidencias");
+            if (!Directory.Exists(rutaFolder))
+                Directory.CreateDirectory(rutaFolder);
+
+            var ruta = Path.Combine(rutaFolder, fileName);
+            if (!alert) { 
+                // Si no hay alerta, usar el método estándar de Selenium
+                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                ss.SaveAsFile(ruta);
+            } else {
+                int width = GetSystemMetrics(0);  // SM_CXSCREEN
+                int height = GetSystemMetrics(1); // SM_CYSCREEN
+
+                Rectangle bounds = new Rectangle(0, 0, width, height);
+
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+                    }
+                    bitmap.Save(ruta);
+                }
+            }
         }
     }
 }
