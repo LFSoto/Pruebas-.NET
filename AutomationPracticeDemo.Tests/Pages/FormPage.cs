241436c980e0137dfc3c8b8ebb3ec88133acae55
koviedoa812 <koviedoa-m@ucenfotec.ac.cr>
2026-03-04 21:14:42 -0600

    Avance práctica clase 3

--- AutomationPracticeDemo.Tests/Pages/FormPage.cs
+++ AutomationPracticeDemo.Tests/Pages/FormPage.cs
@@ -1,4 +1,6 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace AutomationPracticeDemo.Tests.Pages
 {
@@ -11,23 +13,107 @@ namespace AutomationPracticeDemo.Tests.Pages
             _driver = driver;
         }
 
-        private IWebElement NameInput => _driver.FindElement(By.Id("name"));
-        private IWebElement EmailInput => _driver.FindElement(By.Id("email"));
-        private IWebElement PhoneInput => _driver.FindElement(By.Id("phone"));
-        private IWebElement CountryDropdown => _driver.FindElement(By.Id("country"));
-        private IWebElement SubmitButton => _driver.FindElement(By.ClassName("submit-btn"));
+        private IWebElement Signup => _driver.FindElement(By.LinkText("Signup / Login"));
+        private IWebElement Name => _driver.FindElement(By.Name("name"));
+        private IWebElement Email => _driver.FindElement(By.CssSelector("input[data-qa='signup-email']"));
+        private IWebElement SignupButton => _driver.FindElement(By.CssSelector("button[data-qa='signup-button']"));
+        private IWebElement Title => _driver.FindElement(By.Id("id_gender1"));
+        private IWebElement Password => _driver.FindElement(By.Id("password"));
+        private IWebElement FirstName => _driver.FindElement(By.Id("first_name"));
+        private IWebElement LastName => _driver.FindElement(By.Id("last_name"));
+        private IWebElement Company => _driver.FindElement(By.Id("company"));
+        private IWebElement Address => _driver.FindElement(By.Id("address1"));
+        private IWebElement Country => _driver.FindElement(By.Id("country"));
+        private IWebElement State => _driver.FindElement(By.Id("state"));
+        private IWebElement City => _driver.FindElement(By.Id("city"));
+        private IWebElement ZipCode => _driver.FindElement(By.CssSelector("input[data-qa='zipcode']"));
+        private IWebElement MobileNumber => _driver.FindElement(By.CssSelector("input[data-qa='mobile_number']"));
+        private IWebElement btnCreateAccount => _driver.FindElement(By.CssSelector("button[data-qa='create-account']"));
+        private IWebElement Message => _driver.FindElement(By.CssSelector("h2[data-qa='account-created']"));
+        private IWebElement continueButton => _driver.FindElement(By.CssSelector("a[data-qa='continue-button']"));
 
-        public void FillForm(string name, string email, string phone, string country)
+        //HACER CLICK EN SIGNUP
+        public void ClickSignup()
         {
-            NameInput.SendKeys(name);
-            EmailInput.SendKeys(email);
-            PhoneInput.SendKeys(phone);
-            CountryDropdown.SendKeys(country);
+            Signup.Click();
         }
 
-        public void Submit()
+        //COMPLETAR NOMBRE
+        public void NewUser(string name, string email)
         {
-            SubmitButton.Click();
+            Name.SendKeys(name);
+            Email.SendKeys(email);
+
         }
+
+        //HACER CLICK EN SIGNUP
+        public void ClickSignupButton()
+        {
+            SignupButton.Click();
+        }
+
+        //SELECCIONAR TITLE
+        public void SelectTitle()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement titleRadio = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("id_gender1")));
+            Title.Click();
+        }
+
+        //COMPLETAR CONTRASEÑA
+        public void Password1(string password1)
+        {
+            Password.SendKeys(password1);
+        }
+
+        //COMPLETAR INFORMACIÓN
+        public void CompleteInformation(string firstName, string lastName, string company, string address, string country, string state, string city, string zipCode, string MobileNumber1)
+        {
+            FirstName.SendKeys(firstName);
+            LastName.SendKeys(lastName);
+            Company.SendKeys(company);
+            Address.SendKeys(address);
+            Country.SendKeys(country);
+            State.SendKeys(state);
+            City.SendKeys(city);
+            ZipCode.SendKeys(zipCode);
+            MobileNumber.SendKeys(MobileNumber1);
+        }
+
+        //VALIDAR BOTÓN CREAR USUARIO
+        public void ValidateCreateUserButton()
+        {
+            btnCreateAccount.Click();
+        }
+
+        //VALIDAR MENSAJE DE CUENTA CREADA
+        public string ValidateMessage()
+        {
+            return Message.Text;
+
+        }
+
+        //VALIDAR CLIC BTN CONTINUE
+         public void ValidateContinueButton()
+        {
+            continueButton.Click();
+        }
+        
+        //VALIDAR QUE USUARIO ESTÁ LOGUEADO
+        public bool IsUserLoggedIn()
+        {
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+                // Esperamos a que el elemento con el texto "Logged in as" sea visible
+                var element = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[contains(., 'Logged in as')]")));
+                return element.Displayed;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                return false; // Si pasan 10 segundos y no aparece, devuelve false
+            }
+        }
+
     }
 }
--- AutomationPracticeDemo.Tests/Tests/FormTest.cs
+++ AutomationPracticeDemo.Tests/Tests/FormTest.cs
@@ -7,14 +7,123 @@ namespace AutomationPracticeDemo.Tests.Tests
     public class FormTests : TestBase
     {
         [Test]
-        public void Should_FillAndSubmitForm()
+        [Description("Flujo completo de registro de nuevo usuario hasta validación de login")]
+        public void UserRegistrationFlow_ShouldRegisterAndLoginSuccessfully()
         {
+            // 1. Inicialización
             var formPage = new FormPage(Driver);
-            formPage.FillForm("Juan Perez", "juan@test.com", "88888888", "Costa Rica");
-            formPage.Submit();
+            formPage.ClickSignup();
 
-            ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
-            Assert.Pass("Formulario llenado y enviado.");
+            formPage.NewUser("KennethTest", "visual@studio.com");
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 1000);
+            string emailRandom = "PracticaClase3" + randomNumber + "@cenfotec.com";
+
+
+            formPage.ClickSignupButton();
+
+            // 2. Ejecución de pasos (Act)
+            formPage.SelectTitle();
+
+            formPage.Password1("Prueba123456");
+            formPage.CompleteInformation(
+                "Kenneth", "Oviedo", "Microsoft", "Calle Falsa 123",
+                "United States", "California", "Los Angeles", "90001", "1234567890"
+            );
+
+            formPage.ValidateCreateUserButton();
+
+            // 3. Validaciones (Assert)
+            // Usamos Assert.Multiple para validar varias cosas al mismo tiempo
+            Assert.Multiple(() =>
+            {
+                // Validar mensaje de éxito
+                string actualMessage = formPage.ValidateMessage();
+                Assert.That(actualMessage, Is.EqualTo("ACCOUNT CREATED!"), "El mensaje de éxito no coincide");
+
+                // Continuar al área de login
+                formPage.ValidateContinueButton();
+
+                // Validar que el usuario aparece logueado (el elemento de tu imagen)
+                Assert.That(formPage.IsUserLoggedIn(), $"El usuario no aparece como conectado en la cabecera.");
+            });
         }
     }
 }
+
+//[Test]
+//public void Click_SignupTest()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.ClickSignup();
+//    //ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
+//    Assert.Pass("Click en Signup");
+//}
+
+//[Test]
+//public void Enter_Name()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.NewUser("KennethTest", "visual@studio.com");
+//    Assert.Pass("Usuario registrado correctamente");
+//}
+
+//[Test]
+//public void Select_Title()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.SelectTitle();
+//    Assert.Pass("Title seleccionado correctamente");
+//}
+
+//[Test]
+//public void Enter_Password()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.Password1("Prueba123456");
+//    Assert.Pass("Contraseña ingresada correctamente");
+
+//}
+//[Test]
+//public void Information()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.CompleteInformation("Kenneth", "Oviedo", "Microsoft", "Calle Falsa 123", "United States", "California", "Los Angeles", "90001", "1234567890");
+//    Assert.Pass("Información completa ingresada correctamente");
+//}
+
+//[Test]
+//public void Create_Account()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.ValidateCreateUserButton();
+//    Assert.Pass("Cuenta creada correctamente");
+//}
+
+//[Test]
+//public void Validate_Message()
+//{
+//    var formPage = new FormPage(Driver);
+//    string actualMessage = formPage.ValidateMessage();
+//    string expectedMessage = "ACCOUNT CREATED!";
+//    Assert.That(actualMessage, Is.EqualTo(expectedMessage), "El mensaje de éxito no coincide");
+//}
+
+//[Test]
+//public void Validate_btnContinue()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.ValidateContinueButton();
+//    Assert.Pass("Continuar a la cuenta creada correctamente");
+//}
+
+//[Test]
+//public void Validate_Logged()
+//{
+//    var formPage = new FormPage(Driver);
+//    formPage.IsUserLoggedIn();
+//    Assert.Pass("Usuario logueado correctamente");
+//}
+
+
+
