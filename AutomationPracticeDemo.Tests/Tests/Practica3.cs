c6d3d5ad60ea1cdcdcc310c628e5f5d947dd618a
fportuguezc <fportuguezc@ucenfotec.ac.cr>
2026-03-04 21:16:44 -0600

    Avance Practica 3

---
+++ AutomationPracticeDemo.Tests/Tests/Practica3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationPracticeDemo.Tests.Pages;
+using AutomationPracticeDemo.Tests.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutomationPracticeDemo.Tests.Tests
+{
+	public class Practica3 : TestBase
+	{
+		static readonly int random = new Random().Next(1, 1000);
+		string emailRandom = "fran" + random + "@cenfotec.com";
+
+		[Test]
+		public void Caso1_RegistroUsuarioNuevo()
+		{ 
+			var SignupLink = Driver.FindElement(By.CssSelector("a[href='/login']"));
+			SignupLink.Click();
+
+			var NameInput = Driver.FindElement(By.CssSelector("input[data-qa='signup-name']"));
+			NameInput.SendKeys("Fran");
+
+			var EmailInput = Driver.FindElement(By.CssSelector("input[data-qa='signup-email']"));
+			EmailInput.SendKeys(emailRandom);
+
+			var SignupButton = Driver.FindElement(By.CssSelector("button[data-qa='signup-button']"));
+			SignupButton.Click();
+
+			var titleRadiobutton = Driver.FindElement(By.Id("id_gender2"));
+			titleRadiobutton.Click();
+
+			var NameInput2 = Driver.FindElement(By.Id("name"));
+			NameInput2.SendKeys("Fran");
+
+			/*var EmailInput2 = Driver.FindElement(By.Id("email"));
+			EmailInput2.SendKeys(emailRandom);*/
+
+			var Password = Driver.FindElement(By.Id("password"));
+			Password.SendKeys("Fran");
+
+			var Day = Driver.FindElement(By.Id("days"));
+			Day.Click();
+			SelectElement DaySelected = new SelectElement(Day);
+			DaySelected.SelectByValue("1");
+
+
+			var FirstName = Driver.FindElement(By.Id("days"));
+
+			var LastName = Driver.FindElement(By.Id("days"));
+
+			var Company = Driver.FindElement(By.Id("days"));
+
+			var Address = Driver.FindElement(By.Id("days"));
+
+			var Country = Driver.FindElement(By.Id("days"));
+
+			var State = Driver.FindElement(By.Id("days"));
+
+			var City = Driver.FindElement(By.Id("days"));
+
+			var Zipcode = Driver.FindElement(By.Id("days"));
+
+			var MobileNumber = Driver.FindElement(By.Id("days"));
+
+			/*ScreenshotHelper.TakeScreenshot(Driver, "FormDone.png");
+			Assert.Pass("Formulario llenado y enviado.");*/
+		}
+
+	}
+
+}
