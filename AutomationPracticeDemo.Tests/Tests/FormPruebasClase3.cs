ec31f137dfcaa42932d06c68d75582aaac81a3ce
FerSalazar <51107427+FerSalazar@users.noreply.github.com>
2026-03-07 17:38:23 -0600

    Practica 3
    Practica de las clase 3.

--- AutomationPracticeDemo.Tests/Tests/FormPruebasClase3.cs
+++ AutomationPracticeDemo.Tests/Tests/FormPruebasClase3.cs
@@ -1,10 +1,9 @@
-using AutomationPracticeDemo.Tests.Pages;
-using AutomationPracticeDemo.Tests.Utils;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.BiDi.BrowsingContext;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System.Drawing;
 
 namespace AutomationPracticeDemo.Tests.Tests
@@ -17,10 +16,10 @@ namespace AutomationPracticeDemo.Tests.Tests
         {
             var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
-            //options.AddArgument("--headless=new");
-            //options.AddArgument("--window-size=1920,1080");
-            //options.AddArgument("--disable-notifications");
-            //options.AddArgument("--disable-infobars");
+            options.AddArgument("--headless=new");
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--disable-notifications");
+            options.AddArgument("--disable-infobars");
             Driver = new ChromeDriver(options);
             Driver.Navigate().GoToUrl("https://automationexercise.com/");
         }
@@ -42,7 +41,7 @@ namespace AutomationPracticeDemo.Tests.Tests
             var inputName = Driver.FindElement(By.XPath("//input[@data-qa='signup-name']"));
             inputName.SendKeys("Maria");
             var inputEmail = Driver.FindElement(By.XPath("//input[@data-qa='signup-email']"));
-            inputEmail.SendKeys("aria4@qa.com");
+            inputEmail.SendKeys("aria6@qa.com");
             var ingresarBotton = Driver.FindElement(By.XPath("//button[text()='Signup']"));
             ingresarBotton.Click();
             var contrasena = Driver.FindElement(By.Id("password"));
@@ -69,13 +68,17 @@ namespace AutomationPracticeDemo.Tests.Tests
             var mensaje =mensajeCreacion.Text;
             Assert.That(mensaje, Is.EqualTo("ACCOUNT CREATED!"));
             var continuar = Driver.FindElement(By.XPath("//a[@data-qa='continue-button']"));
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementToBeClickable(continuar));
             continuar.Click();
             var validarlogout = Driver.FindElement(By.XPath("//a[text()=' Logout']"));
+            wait.Until(ExpectedConditions.ElementToBeClickable(validarlogout));
             var login = validarlogout.Text;
             Assert.That(login, Is.EqualTo("Logout"));
             var eliminarCuenta = Driver.FindElement(By.XPath("//a[text()=' Delete Account']"));
             eliminarCuenta.Click();
             var continuarEliminacion = Driver.FindElement(By.XPath("//a[@data-qa='continue-button']"));
+            wait.Until(ExpectedConditions.ElementToBeClickable(continuarEliminacion));
             continuarEliminacion.Click();
         }
 
@@ -85,73 +88,123 @@ namespace AutomationPracticeDemo.Tests.Tests
             var singUpBotton = Driver.FindElement(By.XPath("//a[text()=' Signup / Login']"));
             singUpBotton.Click();
             var inputEmail = Driver.FindElement(By.XPath("//input[@data-qa='login-email']"));
-            inputEmail.SendKeys("felipe.soto@email.com");
+            inputEmail.SendKeys("aria@qa.com");
             var inputContrasena = Driver.FindElement(By.XPath("//input[@data-qa='login-password']"));
-            inputContrasena.SendKeys("Password");
+            inputContrasena.SendKeys("123456");
             var ingresarBotton = Driver.FindElement(By.XPath("//button[text()='Login']"));
             ingresarBotton.Click();
             var esperaLogin = new WebDriverWait(Driver, TimeSpan.FromSeconds(15)).Until(a=> a.FindElement(By.XPath("//a[contains(text(),'Logged in as')]")));
             var validarlogout = Driver.FindElement(By.XPath("//a[contains(text(),'Logged in as')]"));
             var login = validarlogout.Text;
-            Assert.That(login, Is.EqualTo("Logged in as Felipe"));
+            Assert.That(login, Is.EqualTo("Logged in as Maria"));
+        }
+        [Test]
+        public void AgregarCarrito ()
+        {
+            var productoBotton = Driver.FindElement(By.XPath("//a[text()=' Products']"));
+            productoBotton.Click();
+            //features_items
+            IList<IWebElement> listaProductos = Driver.FindElements(By.CssSelector(".features_items .col-sm-4"));
+            int producto1 = new Random().Next(0, 33);
+            int producto2 = new Random().Next(0, 33);
+            List<string> nombreProductosAgregados = new List<string>();
+            nombreProductosAgregados.Add(agregarAlCarrito(listaProductos, producto1));
+            ContinuarComprando();
+            nombreProductosAgregados.Add(agregarAlCarrito(listaProductos, producto2));
+            VerCarrito();
+            var tabla = Driver.FindElement(By.Id("cart_info_table"));
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementToBeClickable(tabla));
+            var registros = tabla.FindElements(By.CssSelector("tbody tr"));
+
+            if (registros.Count == 2)
+            {
+                foreach (var producto in registros)
+                {
+                    var nombre = producto.FindElement(By.CssSelector(".cart_description h4")).Text;
+                    if (nombreProductosAgregados.Any(s => s.Equals(nombre))){ 
+                        var cantidad = producto.FindElement(By.ClassName("cart_quantity")).Text;
+                        var precio = producto.FindElement(By.ClassName("cart_price")).Text.Replace("Rs. ", "");
+                        var total = producto.FindElement(By.ClassName("cart_total")).Text.Replace("Rs. ", "");
+                        var calculo = Convert.ToInt32(cantidad) * Convert.ToInt32(precio);
+                        Assert.That(calculo.ToString(), Is.EqualTo(total));
+                    } else {
+                        Assert.Fail("No se encuentra el producto en el carrito");
+                    }
+                }
+                Assert.Pass("Se encontraron los productos y calculo de total correctos.");
+            } else
+                Assert.Fail("No contiene los productos agregados.");
+
+        }
+
+        private string agregarAlCarrito(IList<IWebElement> listaProductos, int i)
+        {
+            var nombreProducto = listaProductos[i].FindElement(By.CssSelector("div.productinfo p")).Text;
+            IWebElement productoBotton = listaProductos[i].FindElement(By.XPath(".//a[text()='Add to cart']"));
+            // Hacer scroll hasta el botón
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", productoBotton);
+            // Forzar el clic con JavaScript para evitar overlays
+            js.ExecuteScript("arguments[0].click();", productoBotton);
+            return nombreProducto;
+        }
+
+        private void ContinuarComprando()
+        {
+            var continuarShopping = Driver.FindElement(By.CssSelector("#cartModal button"));
+            // Espera a que el modal de confirmación aparezca para dar click
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(continuarShopping));
+            continuarShopping.Click();
+        }
+        private void VerCarrito()
+        {
+            var verCarrito = Driver.FindElement(By.CssSelector("#cartModal a[href*='/view_cart']"));
+            // Espera a que el modal de confirmación aparezca para dar click
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(verCarrito));
+            verCarrito.Click();
+        }
+        [Test]
+        public void FormularioContacto()
+        {
+            var contactoBoton = Driver.FindElement(By.XPath("//a[text()=' Contact us']"));
+            contactoBoton.Click();
+            var nombre = Driver.FindElement(By.XPath("//input[@data-qa='name']"));
+            nombre.SendKeys("Maria");
+            var email = Driver.FindElement(By.XPath("//input[@data-qa='email']"));
+            email.SendKeys("maria@qa.com");
+            var asunto = Driver.FindElement(By.XPath("//input[@data-qa='subject']"));
+            asunto.SendKeys("Ayuda");
+            var mensaje = Driver.FindElement(By.XPath("//textarea[@data-qa='message']"));
+            mensaje.SendKeys("Ayuda no entiendo como se usa.");
+            var rutaFolder = Path.GetFullPath(@"..\..\..\ArchivoAdjunto\226531.jpg");
+            var archivo = Driver.FindElement(By.XPath("//input[@name='upload_file']"));
+            archivo.SendKeys(rutaFolder);
+            var submintBoton = Driver.FindElement(By.XPath("//input[@data-qa='submit-button']"));
+            submintBoton.Click();
+            var alert = Driver.SwitchTo().Alert();
+            alert.Accept();
+            var mensajeExito = Driver.FindElement(By.CssSelector(".status.alert.alert-success"));
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(mensajeExito));
+            var validarMensaje = mensajeExito.Text;
+            Assert.That(validarMensaje, Is.EqualTo("Success! Your details have been submitted successfully."));
+        }
+        [Test]
+        public void Suscripcion()
+        {
+            var ingresarEmail = Driver.FindElement(By.Id("susbscribe_email"));
+            ingresarEmail.SendKeys("aria@qa.com");
+            var botonEnviar = Driver.FindElement(By.Id("subscribe"));
+            botonEnviar.Click();
+            var mensajeExito = Driver.FindElement(By.CssSelector(".alert-success.alert"));
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(mensajeExito));
+            var validarMensaje = mensajeExito.Text;
+            Assert.That(validarMensaje, Is.EqualTo("You have been successfully subscribed!"));
         }
     }
-
-        //[Test]
-        //public void Should_FillAndSubmitForm()
-        //{
-        //    var formPage = new FormPage(Driver);
-        //    formPage.FillForm("Juan Perez", "juan@test.com", "88888888", "Costa Rica");
-        //    formPage.Submit();
-
-        //    ScreenshotHelper.TakeScreenshot(Driver, "form_test.png");
-        //    Assert.Pass("Formulario llenado y enviado.");
-        //}
-        //[Test]
-        //public void VerificarRadioButtom_Genero()
-        //{
-        //    var formPage = new FormPage(Driver);
-        //    formPage.FillGender("Male");
-
-        //    ScreenshotHelper.TakeScreenshot(Driver, "form_RadioButtom.png");
-        //    Assert.Pass("Seleccionado el genero correctamente");
-        //}
-        //[Test]
-        //public void VerificarCheckBox_Days()
-        //{
-        //    var formPage = new FormPage(Driver);
-        //    string[] dias = { "monday", "friday", "sunday" };
-        //    foreach (var dia in dias){
-        //        formPage.FillDays(dia);
-        //    }
-
-        //    ScreenshotHelper.TakeScreenshot(Driver, "form_CheckBoxDays.png");
-        //    Assert.Pass($"Seleccionado los dias {string.Join(",", dias)} correctamente");
-        //}
-        //[Test]
-        //public void VerificarRangeDates()
-        //{
-        //    var formPage = new FormPage(Driver);
-        //    formPage.FillRangeDates(new DateTime(2026,02,03).ToString("dd-MM-yyyy"), new DateTime(2026, 03, 03).ToString("dd-MM-yyyy"));
-        //    formPage.SubmitDates();
-
-        //    ScreenshotHelper.TakeScreenshot(Driver, "form_RangeDate.png");
-        //    Assert.Pass("Seleccionado el rango de fechas y submit.");
-        //}
-        //[Test]
-        //public void VerificarPromptAlert()
-        //{
-        //    var formPage = new FormPage(Driver);
-        //    formPage.OpenAlertPrompt();
-        //    // Esperar un instante para que el alerta aparezca
-        //    Thread.Sleep(200);
-        //    ScreenshotHelper.TakeScreenshot(Driver, "form_PromptAlert.png",true);
-
-        //    IAlert alert = Driver.SwitchTo().Alert();
-        //    string actualAlertText = alert.Text;
-        //    Assert.That(actualAlertText, Is.EqualTo("Please enter your name:"));
-        //    alert.Accept();
-
-        //}
- }
+}
 
--- README.md
+++ README.md
@@ -1,22 +1,23 @@
-# Proyecto de Pruebas Automatizadas - Automation Practice Demo
+## Pasos para empezar
 
-Este proyecto contiene un esqueleto en .NET con NUnit y Selenium para practicar pruebas funcionales sobre el sitio [Automation Testing Practice](https://testautomationpractice.blogspot.com/).
+1. **Clonar el repositorio**
+   ```bash
+   git clone https://github.com/LFSoto/Pruebas-.NET.git
+   cd AutomationPracticeDemo.Tests
 
-## Requisitos
-- .NET 8 SDK
-- Google Chrome
-- ChromeDriver (instalado automáticamente por NuGet)
+2. **Restaurar dependencias**
+	```bash
+   dotnet restore
 
-## Instalación
-```bash
-git clone https://github.com/LFSoto/Pruebas-.NET.git
-cd AutomationPracticeDemo.Tests
-dotnet restore
-dotnet test
-```
-## Pruebas realizadas
-1. Prueba elemento web RadioButton de selección de genero, captura de pantalla usando el método estándar de Selenium.
-2. Prueba elemento web CheckBox de selección de días de la semana, captura de pantalla usando el método estándar de Selenium.
-3. Prueba elemento web DatePicker selección de rango de fechas (inicio y fin) invocando el evento click del botón Submit para calculo de diferencia de días, captura de pantalla usando el método estándar de Selenium.
-4. Prueba elemento web Alerts "prompt alert" invocando el evento click para abrir la alerta y verificar el texto "Please enter your name:", captura de pantalla utilizando una captura de pantalla completa por inpedimento al utilizar el metodo estandar de selenium al tener una alerta abierta.
+  ## Comandos útiles
+
+1. Compilar:
+	```bash
+	dotnet build
+
+2. Ejecutar pruebas:
+	```bash
+	dotnet test
+
+
 
