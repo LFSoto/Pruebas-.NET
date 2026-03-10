5f244e7fba77aa4b633855b18146954038117268
smunozv-pixel <smunozv@ucenfotec.ac.cr>
2026-03-09 23:33:57 -0600

    Ajustes Punto 4

--- AutomationPracticeDemo.Tests/Utils/TestBase.cs
+++ AutomationPracticeDemo.Tests/Utils/TestBase.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace AutomationPracticeDemo.Tests.Utils
 {
@@ -91,33 +92,66 @@ namespace AutomationPracticeDemo.Tests.Utils
             System.Threading.Thread.Sleep(2000);
             //silvia paso#3 Click en login
             Driver.FindElement(By.CssSelector("[data-qa='login-button']")).Click();
-            System.Threading.Thread.Sleep(3000);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            
 
             //AGREGAR PRODUCTOS AL CARRITO
             // silvia Paso #1 Ir a Products
-        
             Driver.FindElement(By.CssSelector("a[href='/products']")).Click();
             System.Threading.Thread.Sleep(3000);
             // Silvia Paso 2: Agregar dos productos al carrito
             Driver.FindElement(By.CssSelector("a[data-product-id='1']")).Click();
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
             Driver.FindElement(By.CssSelector(".btn.btn-success.close-modal.btn-block")).Click();
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
             Driver.FindElement(By.CssSelector("a[data-product-id='14']")).Click();
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
             Driver.FindElement(By.CssSelector(".btn.btn-success.close-modal.btn-block")).Click();
             System.Threading.Thread.Sleep(5000);
-
+            IJavaScriptExecutor ct = (IJavaScriptExecutor)Driver;
             // Silvia Paso 3: Click en View Cart
             Driver.FindElement(By.CssSelector("a[href='/view_cart']")).Click();
             System.Threading.Thread.Sleep(3000);
-
             // Silvia Paso 4: Validar que los productos aparecen en la tabla y que el total se muestra correctamente
             var productos = Driver.FindElements(By.CssSelector("#cart_info_table tbody tr"));
             Assert.AreEqual(2, productos.Count, "Deberían aparecer 2 productos en el carrito.");
             System.Threading.Thread.Sleep(3000);
             var montoTotal = Driver.FindElement(By.XPath("//td[@class='total']/following-sibling::td")).Text;
-            System.Threading.Thread.Sleep(3000); 
+            System.Threading.Thread.Sleep(3000);
+
+             //US FORM
+            //Silvia Paso 1: Ir a Contact Us.
+            Driver.FindElement(By.CssSelector("a[href='/contact_us']")).Click();
+            System.Threading.Thread.Sleep(1000);
+            IJavaScriptExecutor cu = (IJavaScriptExecutor)Driver;
+            //Silvia Paso 2: Completar nombre, email, asunto y mensaje. 
+            Driver.FindElement(By.Name("name")).SendKeys("Silvia");
+            System.Threading.Thread.Sleep(1000);
+            Driver.FindElement(By.Name("email")).SendKeys("silvia88cr12@gmail.com");
+            System.Threading.Thread.Sleep(1000);
+            Driver.FindElement(By.Name("subject")).SendKeys("Test Silvia");
+            System.Threading.Thread.Sleep(1000);
+            Driver.FindElement(By.Id("message")).SendKeys("Prueba paso #4.");
+            System.Threading.Thread.Sleep(1000);
+            //Silvia Paso 3: Adjuntar un archivo. 
+            Driver.FindElement(By.Name("upload_file")).SendKeys(@"C:\Users\ofgm1\source\repos\Test.txt");
+            System.Threading.Thread.Sleep(1000);
+            // Silvia Paso 5: Click en Submit
+            Driver.FindElement(By.Name("submit")).Click();
+            System.Threading.Thread.Sleep(1000);
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+            IAlert alert = wait.Until(ExpectedConditions.AlertIsPresent());
+            alert.Accept(); // Presiona OK en el alert
+
+            //Silvia Paso 5: Mensaje de exito 
+            WebDriverWait wait1 = new WebDriverWait(Driver, TimeSpan.FromSeconds(2));
+            var mensajeExito = wait1.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".status.alert.alert-success"))).Text;
+            Assert.AreEqual("Success! Your details have been submitted successfully.", mensajeExito);
+
+
+
+
+
 
         }
 
